ae26b0620a37cb7144b2df94cf582c92e995f4dc
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-01 16:22:41 -0700

    Updates

---
+++ precache-manifest.6e3ab06df6d84996f2562b95f723f125.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af8ee95fded80d02435bcbb14c022e9",
+    "url": "%7Busername%7D.github.io/event-logger/index.html"
+  },
+  {
+    "revision": "23c1f511d870befb59ae",
+    "url": "%7Busername%7D.github.io/event-logger/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "5d6aa9b9d4c9842d45b6",
+    "url": "%7Busername%7D.github.io/event-logger/static/css/main.362d94d5.chunk.css"
+  },
+  {
+    "revision": "23c1f511d870befb59ae",
+    "url": "%7Busername%7D.github.io/event-logger/static/js/2.7bebcd4c.chunk.js"
+  },
+  {
+    "revision": "fe90904e5d8f00450281",
+    "url": "%7Busername%7D.github.io/event-logger/static/js/3.9d509fbb.chunk.js"
+  },
+  {
+    "revision": "5d6aa9b9d4c9842d45b6",
+    "url": "%7Busername%7D.github.io/event-logger/static/js/main.28998a90.chunk.js"
+  },
+  {
+    "revision": "0e01f1adfe6e6253db36",
+    "url": "%7Busername%7D.github.io/event-logger/static/js/runtime-main.c5e4edd6.js"
+  }
+]);
